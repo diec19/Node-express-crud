50fe47135f57e2da26687acccfad39859d94feba
diec19 <diec.cas19@gmail.com>
2023-05-10 20:26:14 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongosee = require('mongoose')
+require("dotenv").config()
+const userRoutes = require("./routes/user")
+
+const app= express();
+const port = process.env.PORT || 9000;
+
+//middleware
+app.use('/api',userRoutes)
+
+// routes
+app.get('/',(req,res)=>{
+    res.send("Welcome to my API");
+})
+
+//mongodb connection
+mongosee.connect(process.env.MONGODB_URI)
+.then(()=> console.log("conectado a mongo DB"))
+.catch((error)=> console.log(error))
+
+app.listen(9000,()=> console.log('server listeng on port',port))
